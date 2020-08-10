e1b3587cc635fb13bea1c9f3160fab356cf5733c
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-08-10 14:57:39 +0700

    init project

---
+++ js.js
@@ -0,0 +1,12 @@
+function myFunction(){
+    // alert("Xin chao Nam Anh");
+    // // console.log("Xin chao C0720I1");
+    // let name = document.getElementById("name").value;
+    // let xacnhan = confirm("Bạn có phải nam không?")
+    // document.getElementById("result").innerText = " Tên là " +  name + "Nam? " + xacnhan;
+    // document.write(name);
+    let name = prompt("Mời bạn nhập vào tên");
+    // console.log(name);
+    document.getElementById("name").value = name;
+
+}
